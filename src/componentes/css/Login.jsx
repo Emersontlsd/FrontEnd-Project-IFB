42462c2f444763610ddf7ab3cf30a325e3636c42
Emersontlsd <emersonsdom13@hotmail.com>
2025-10-01 09:54:42 -0300

    change line 22

--- src/componentes/css/Login.jsx
+++ src/componentes/css/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
 
                 <div className="form-group">
                     <Label texto="Senha:" />
-                    <InputText placeholder="password..." />
+                    <InputPassword placeholder="password..." />
                 </div>
 
                 <Button valor="Logar" />
